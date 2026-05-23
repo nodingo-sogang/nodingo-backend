7fcea0be690f7898f9d41e21bbcc946af307863b
EauLune01 <161578205+EauLune01@users.noreply.github.com>
2026-05-23 09:09:18 +0900

    Refactor: async method for user onboarding
    Co-authored-by: 최성민 <smsm7288@gmail.com>

--- src/main/java/nodingo/core/global/config/async/AsyncConfig.java
+++ src/main/java/nodingo/core/global/config/async/AsyncConfig.java
@@ -1,20 +1,21 @@
 package nodingo.core.global.config.async;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-@Configuration
-public class AsyncConfig {
+import java.util.concurrent.Executor;
 
-    @Bean(name = "embeddingTaskExecutor")
-    public TaskExecutor embeddingTaskExecutor() {
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(30);
-        executor.setMaxPoolSize(50);
-        executor.setQueueCapacity(1000);
-        executor.setThreadNamePrefix("Embedding-Async-");
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("async-onboarding-");
         executor.initialize();
         return executor;
     }
