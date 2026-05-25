4230dc7e9e590c19296671977aeb6a9448133ab8
최성민 <smsm7288@gmail.com>
2026-05-25 13:53:02 +0900

    chore: clean notificationcontroller

--- src/main/java/nodingo/core/notification/controller/NotificationController.java
+++ src/main/java/nodingo/core/notification/controller/NotificationController.java
@@ -18,15 +18,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
 @Tag(name = "Notification", description = "알림 관련 API")
 @RestController
 @RequestMapping("/api/users/notifications")
