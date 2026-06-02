14320220dac6adf04eb735ff13062b1988708dde
EauLune01 <161578205+EauLune01@users.noreply.github.com>
2026-06-02 16:37:19 +0900

    refactor: add new method to keywordrelationrepository and some logs to NEwsAiWriter
    Co-authored-by: Pierre <smsm7288@gmail.com>

--- src/main/java/nodingo/core/keyword/repository/custom/KeywordRelationRepositoryCustom.java
+++ src/main/java/nodingo/core/keyword/repository/custom/KeywordRelationRepositoryCustom.java
@@ -5,8 +5,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface KeywordRelationRepositoryCustom {
     Slice<KeywordRelation> findTopRelations(Long keywordId, Pageable pageable);
     List<KeywordRelation> findAllRelationsIn(List<Long> keywordIds);
+    Optional<KeywordRelation> findByPair(Long subjectId, Long relatedId);
 }
