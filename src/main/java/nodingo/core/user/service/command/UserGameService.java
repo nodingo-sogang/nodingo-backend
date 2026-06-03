0c36d52d2653c340deef939fa14f9f0a395dcd6b
Pierre <smsm7288@gmail.com>
2026-06-03 11:11:13 +0900

    refactor: updateUserGameService

--- src/main/java/nodingo/core/user/service/command/UserGameService.java
+++ src/main/java/nodingo/core/user/service/command/UserGameService.java
@@ -1,6 +1,7 @@
 package nodingo.core.user.service.command;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import nodingo.core.global.exception.user.UserNotFoundException;
 import nodingo.core.user.domain.User;
 import nodingo.core.user.repository.UserRepository;
@@ -8,6 +9,10 @@ import nodingo.core.user.utils.GamePolicy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -18,7 +23,16 @@ public class UserGameService {
 
     public void checkAndRewardAttendance(Long userId) {
         User user = getUserOrElseThrow(userId);
-        ifFirstVisit(user);
+
+        LocalDate standardToday = getStandardToday();
+
+        ifFirstVisit(user, standardToday);
+    }
+
+    private static LocalDate getStandardToday() {
+        return LocalTime.now().isBefore(LocalTime.of(5, 0))
+                ? LocalDate.now().minusDays(1)
+                : LocalDate.now();
     }
 
     private User getUserOrElseThrow(Long userId) {
@@ -26,9 +40,11 @@ public class UserGameService {
                 .orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다."));
     }
 
-    private void ifFirstVisit(User user) {
-        if (user.recordAttendance()) {
+    private void ifFirstVisit(User user, LocalDate standardToday) {
+        if (user.recordAttendance(standardToday)) {
             user.addXp(gamePolicy.getFirstVisitXp());
+            log.info(">>>> [Attendance Reward] User {} checked in for date: {}. Earned {} XP",
+                    user.getId(), standardToday, gamePolicy.getFirstVisitXp());
         }
     }
 }
