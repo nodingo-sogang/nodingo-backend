da6158eee1e99e23ce5e440161145a65d4966e2a
Pierre <smsm7288@gmail.com>
2026-06-04 13:09:07 +0900

    refactor: update authwithdraw

--- src/main/java/nodingo/core/friendship/repository/FriendshipRepository.java
+++ src/main/java/nodingo/core/friendship/repository/FriendshipRepository.java
@@ -2,7 +2,12 @@ package nodingo.core.friendship.repository;
 import nodingo.core.friendship.domain.Friendship;
 import nodingo.core.friendship.repository.custom.FriendshipRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FriendshipRepository extends JpaRepository<Friendship, Long>, FriendshipRepositoryCustom {
-
+    @Modifying
+    @Query("delete from Friendship f where f.requester.id = :requesterId or f.receiver.id = :receiverId")
+    void deleteByRequesterIdOrReceiverId(@Param("requesterId") Long requesterId, @Param("receiverId") Long receiverId);
 }
