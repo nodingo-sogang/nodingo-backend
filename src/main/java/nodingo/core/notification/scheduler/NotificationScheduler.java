8c0a9a08a81c62f6da5d64816d2189ab323bd9b4
Pierre <smsm7288@gmail.com>
2026-06-04 10:37:28 +0900

    chore: clean notificationscheduler

--- src/main/java/nodingo/core/notification/scheduler/NotificationScheduler.java
+++ src/main/java/nodingo/core/notification/scheduler/NotificationScheduler.java
@@ -13,18 +13,10 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.List;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Slf4j
