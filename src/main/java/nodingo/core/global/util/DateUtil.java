c9860e0ff557ff939abe7287cfb2169e13f7cbb8
Pierre <smsm7288@gmail.com>
2026-06-09 11:18:00 +0900

    bug: fix DateUtil

--- src/main/java/nodingo/core/global/util/DateUtil.java
+++ src/main/java/nodingo/core/global/util/DateUtil.java
@@ -1,25 +1,23 @@
 package nodingo.core.global.util;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.ZoneId;
 
 public class DateUtil {
 
     private DateUtil() {}
 
     public static LocalDate getNow() {
-        return LocalDate.now(ZoneId.of("Asia/Seoul"));
+        return LocalDate.now();
     }
 
     public static LocalDate getTargetDate() {
-        return LocalTime.now(ZoneId.of("Asia/Seoul")).isBefore(LocalTime.of(5, 0))
-                ? LocalDate.now(ZoneId.of("Asia/Seoul")).minusDays(1)
-                : LocalDate.now(ZoneId.of("Asia/Seoul"));
+        return LocalTime.now().isBefore(LocalTime.of(5, 0))
+                ? LocalDate.now().minusDays(1)
+                : LocalDate.now();
     }
 
     public static LocalDate getMinusOneDay() {
-        return LocalDate.now(ZoneId.of("Asia/Seoul")).minusDays(1);
+        return LocalDate.now().minusDays(1);
     }
 }
